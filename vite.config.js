878cc3d217ddddb4a3f836d280784d5d5ca681ef
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-12 22:05:13 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/K72/',
+  base: '/K72/', // 👈 must match EXACTLY your repo name (case-sensitive)
 })
