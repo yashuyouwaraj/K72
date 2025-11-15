be21fd69a3d024c9bdd378cc5069b2ca28fc3065
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-15 22:17:29 +0530

    Stairs

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import Agence from "./pages/Agence"
 import Projects from "./pages/Projects"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { useRef } from "react"
 
 const App = () => {
+
   return (
     <div className="text-white">
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,7 @@
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video autoPlay loop muted className="object-cover h-full w-full" src="https://player.vimeo.com/progressive_redirect/playback/1119600858/rendition/1080p/file.mp4?loc=external&log_user=0&signature=c4a137161d6ce80a52c50c7ee23d4fdf8df103bfc816252fc304e317a43bacc6"></video>
+      <video autoPlay loop muted className="object-cover h-full w-full" src="src/assets/69496b2d.mp4"></video>
     </div>
   )
 }
